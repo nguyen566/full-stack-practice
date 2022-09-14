e53993d26c88a333ea68c9f2a1d8c770b56358ae
nguyen566 <randy.nguyen@live.com>
2022-09-14 09:33:10 -0500

    Setting up CRUD w/MikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,40 @@
-import { MikroORM, RequestContext } from "@mikro-orm/core";
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
   await orm.getMigrator().up();
 
-  await RequestContext.createAsync(orm.em, async () => {
-    // const post = orm.em.create(Post, { title: "my first post" });
-    // await orm.em.persistAndFlush(post);
-    const post = await orm.em.find(Post, {});
-    console.log(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em.fork({}) })
+  });
+
+  //sets middle ware to create GraphQL endpoint to express
+  apolloServer.applyMiddleware({ app });
+
+  //using underscore can allow you to ignore a parameter
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
   });
 };
 
 main().catch((err) => {
   console.log(err);
 });
+
+//new way of contacting postgres database for v5+
+// await RequestContext.createAsync(orm.em, async () => {
+// });

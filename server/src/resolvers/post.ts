dcfa79b67921da623ec198c6c5474c5bf39ca342
nguyen566 <randy.nguyen@live.com>
2022-09-28 09:49:53 -0500

    rollback

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -53,20 +53,51 @@ export class PostResolver {
 		const realValue = isUpdoot ? 1 : -1;
 		const userId = req.session!.userId;
 
-		await getConnection().query(
-			`
-			START TRANSACTION;
+		const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-			insert into updoot ("userId", "postId", "value")
-			values(${userId}, ${postId}, ${realValue});
+		//user has voted on post before and changing vote
+		if (updoot && updoot.value !== realValue) {
+			await getConnection().transaction(async (tm) => {
+				await tm.query(
+					`
+					update updoot
+					set value = $1
+					where "postId" = $2 and "userId" = $3
+					`,
+					[realValue, postId, userId]
+				);
+				await tm.query(
+					`
+				update post 
+				set points = points + $1
+				where id = $2
+				`,
+					[2*realValue, postId]
+				);
+			});
+			//user does not have a vote
+		} else if (!updoot) {
+			//Transaction error requires rolling back if error (typeorm can handle)
+			await getConnection().transaction(async (tm) => {
+				tm.query(
+					`
+				insert into updoot ("userId", "postId", "value")
+				values($1, $2, $3)
+				`,
+					[userId, postId, realValue]
+				);
 
-			update post 
-			set points = points + ${realValue}
-			where id = ${postId};
+				await tm.query(
+					`
+				update post 
+				set points = points + $1
+				where id = $2
+				`,
+					[realValue, postId]
+				);
+			});
+		}
 
-			COMMIT;
-			`
-		);
 		return true;
 	}
 
